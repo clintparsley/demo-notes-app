17e80fab36eb7ca80dc4e89588eaa40f7061bc5e
Clint Parsley <clint.parsley@gmail.com>
2022-11-27 21:56:44 -0700

    Update get.js
    update: setting back to normal

--- services/functions/get.js
+++ services/functions/get.js
@@ -5,11 +5,9 @@ export const main = handler(async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME,
     // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
-      noteId: event.pathParameters.id,
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
+      noteId: event.pathParameters.id, // The id of the note from the path
     },
   };
 
@@ -17,9 +15,6 @@ export const main = handler(async (event) => {
   if (!result.Item) {
     throw new Error("Item not found.");
   }
-
-  // Set a timeout
-  await new Promise((resolve) => setTimeout(resolve, 10000));
 
   // Return the retrieved item
   return result.Item;
